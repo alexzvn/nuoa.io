13d5339c10fdfb34bf4b7d57bcde169d70475693
Alexzvn <personal@alexzvn.me>
2024-08-05 16:27:48 +0700

    wip

--- components/DataViewPaginate.vue
+++ components/DataViewPaginate.vue
@@ -11,28 +11,30 @@
     </div>
 
     <hr>
-    <div class="py-3 px-4 flex justify-between">
-      <span></span>
+    <slot name="footer">
+      <div class="py-3 px-4 flex justify-between">
+        <span></span>
 
-      <div class="flex text-sm opacity-90">
-        <div class="mt-1.5">
-          Row Per Page:
-          <select v-model="selected" class="select select-xs">
-            <option v-for="item in perPage" :value="item" :selected="item === take">{{ item }}</option>
-          </select>
-        </div>
+        <div class="flex text-sm opacity-90">
+          <div class="mt-1.5">
+            Row Per Page:
+            <select v-model="selected" class="select select-xs">
+              <option v-for="item in perPage" :value="item" :selected="item === take">{{ item }}</option>
+            </select>
+          </div>
 
-        <div class="flex space-x-2">
-          <button class="btn btn-sm btn-circle btn-ghost">
-            <Icon name="fe:arrow-left" class="size-5" @click="emit('prev')" />
-          </button>
-          <span class="mt-1.5">1-3 of 3</span>
-          <div class="btn btn-sm btn-circle btn-ghost" @click="emit('next')">
-            <Icon name="fe:arrow-right" class="size-5" />
+          <div class="flex space-x-2">
+            <button class="btn btn-sm btn-circle btn-ghost">
+              <Icon name="fe:arrow-left" class="size-5" @click="emit('prev')" />
+            </button>
+            <span class="mt-1.5">1-3 of 3</span>
+            <div class="btn btn-sm btn-circle btn-ghost" @click="emit('next')">
+              <Icon name="fe:arrow-right" class="size-5" />
+            </div>
           </div>
         </div>
       </div>
-    </div>
+    </slot>
   </div>
 </template>
 
---
+++ pages/me/incoming/index.vue
@@ -0,0 +1,46 @@
+<template>
+  <DataViewPaginate class="min-h-[60vh]">
+    <template #title>
+      <div class="flex justify-between space-x-3 px-3">
+        <h3 class="card-title font-bold">Current Requests</h3>
+        <NuxtLink to="/me/published/create" class="btn btn-sm btn-success">New PCF</NuxtLink>
+      </div>
+      <hr>
+    </template>
+
+    <table class="table">
+      <thead>
+        <tr>
+          <th>Request No</th>
+          <th>Requester Name</th>
+          <th>PCF ID</th>
+          <th>Product ID</th>
+          <th>Product Name</th>
+          <th>Requested Date</th>
+          <th>Action</th>
+        </tr>
+      </thead>
+      <tbody>
+
+        <tr class="hover">
+          <th>1</th>
+          <td>Company A</td>
+          <td>abc1</td>
+          <td>product1</td>
+          <td>Door Car</td>
+          <td>07/05/2024</td>
+          <td>
+            <NuxtLink to="/me/incoming/manage" class="link text-blue-600" style="display: block; width: 5rem;">
+              <p>View detail</p>
+            </NuxtLink>
+          </td>
+        </tr>
+
+      </tbody>
+    </table>
+  </DataViewPaginate>
+</template>
+
+<script lang="ts" setup>
+import DataViewPaginate from '~/components/DataViewPaginate.vue'
+</script>
